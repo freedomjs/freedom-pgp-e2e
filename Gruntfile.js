aac09826fccbfd5e613be55ac1c68800da165527
soycode <soycode@users.noreply.github.com>
2015-04-02 11:19:17 -0700

    adding release scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -101,6 +101,44 @@ module.exports = function(grunt) {
       }
     },
 
+    bump: {
+      options: {
+        files: ['package.json'],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin'
+      }
+    },
+
+    'npm-publish': {
+      options: {
+        // list of tasks that are required before publishing
+        requires: [],
+        // if the workspace is dirty, abort publishing (to avoid publishing local changes)
+        abortIfDirty: true
+      }
+    },
+
+    prompt: {
+      tagMessage: {
+        options: {
+          questions: [
+            {
+              config: 'bump.options.tagMessage',
+              type: 'input',
+              message: 'Enter a git tag message:',
+              default: 'v%VERSION%'
+            }
+          ]
+        }
+      }
+    },
+
     clean: ['build/', '.build/']
   });
 
@@ -126,6 +164,16 @@ module.exports = function(grunt) {
     'build',
     'jasmine_node'
   ]);
+  grunt.registerTask('release', function (arg) {
+    if (arguments.length === 0) {
+      arg = 'patch';
+    }
+    grunt.task.run([
+      'prompt:tagMessage',
+      'bump:' + arg,
+      'npm-publish'
+    ]);
+  });
   grunt.registerTask('demo', [
     'build',
     'connect'
