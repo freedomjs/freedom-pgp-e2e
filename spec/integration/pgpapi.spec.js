ce9308a18d02093f1cc307d79001ac1ca996a86a
soycode <soycode@users.noreply.github.com>
2015-11-09 14:26:35 -0800

    longer test timeout

--- spec/integration/pgpapi.spec.js
+++ spec/integration/pgpapi.spec.js
@@ -52,7 +52,7 @@ var privateKeyStr =
     '=H/6h\r\n' +
     '-----END PGP PRIVATE KEY BLOCK-----\r\n';
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 
 describe('PGP api integration', function() {
   'use strict';
